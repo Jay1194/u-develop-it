722febda219e1c0d7cbee8531691c3a15933f104
Jay <j.thomastt1@gmail.com>
2023-10-29 09:10:44 -0600

    Create MySQL Queries for Read, Delete, and Create Operations

--- server.js
+++ server.js
@@ -31,11 +31,47 @@ const db = mysql.createConnection(
 //In the preceding statement, the db object is using the query() method. This method runs the SQL query and executes the callback with all the resulting rows that match the query.
 //Once this method executes the SQL command, the callback function captures the responses from the query in two variables: the err, which is the error response, and rows, which is the database query response. If there are no errors in the SQL query, the err value is null.
 //This method is the key component that allows SQL commands to be written in a Node.js application.
-
-//query the database to test the connection
-db.query(`SELECT * FROM candidates`, (err, rows) => {
+//query the database to test the connection - gets all candidates OPERATIONS
+/*db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
+*/
+
+/*
+//GET a single candidate - hardcoded OPERATIONS
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row ) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+})
+*/
+//Delete a candidate - ?) that denotes a placeholder, making this a prepared statement. A prepared statement can execute the same SQL statements repeatedly using different values in place of the placeholder. - hardcoding 1 temporarily to demonstrate how prepared statements work. So this would be the same as saying DELETE FROM candidates WHERE id = 1. - the param argument can be an array that holds multiple values for the multiple placeholders.
+/*db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+*/
+
+
+/*
+// Create Query for Create Operation
+//In the SQL command we use the INSERT INTO command for the candidates table to add the values that are assigned to params. The four placeholders must match the four values in params, so we must use an array.
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?,?,?,?)`;
+
+//Because the candidates table includes four columns—id, first_name, last_name, and industry_connected—we need four placeholders (?) for those four values. The values in the params array must match the order of those placeholders
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err) ;
+        }
+        console.log(result);
+})
+*/
 
 //handle user request not supported by the app - Default response for any other request (Not Found)  ----------always last route!!!!
 app.use((req, res) => {
